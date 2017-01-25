7302d5ad755fdf012676be9077fe1ed658a2e1f3
julien <julien@papier-code.fr>
2017-01-25 16:00:56 +0100

    Ajout plugin TinyMce Media (suite)

--- pc-custom-wp.php
+++ pc-custom-wp.php
@@ -293,6 +293,7 @@ add_filter( 'mce_external_plugins', function( $plugins ) {
 
     $plugins['visualblocks'] = plugin_dir_url( __FILE__ ).'tinymce/visualblocks/plugin.min.js';
     $plugins['pcanchor'] = plugin_dir_url( __FILE__ ).'tinymce/pc-plugin_anchor/pc-plugin_anchor.js';
+    $plugins['pcembed'] = plugin_dir_url( __FILE__ ).'tinymce/media/plugin.min.js';
     return $plugins;
 
 });
@@ -319,11 +320,18 @@ add_filter( 'tiny_mce_before_init', function( $settings ) use( $pcSettings ) {
 	    // echo '</pre>';
 	    // exit();
 
+        // contenu des barres d'outils
         $settings['toolbar1']                     = $pcSettings['tinymce-toolbar1'];
         $settings['toolbar2']                     = $pcSettings['tinymce-toolbar2'];
+        // menu type de block
         $settings['block_formats']                = $pcSettings['tinymce-block'];
+        // plugin visual block activation
         $settings['visualblocks_default_state']   = ($pcSettings['tinymce-visualblocks'] == 1 ? true : false);
+        // copier comme texte activation
         $settings['paste_as_text']                = ($pcSettings['tinymce-paste'] == 1 ? true : false);
+        // options plugin media
+        $settings['media_alt_source']             = false;
+        $settings['media_poster']                 = false;
 
     }
 
