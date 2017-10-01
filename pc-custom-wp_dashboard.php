92cf57f1bcd0f6319c051c09bec84e303ca98469
julien <julien@papier-code.fr>
2017-10-01 19:42:00 +0200

    V0.5.6 suppresion choix type de commentaire
    + affichage box activité sur le dashboard si le menu des commentaires est activé

--- pc-custom-wp_dashboard.php
+++ pc-custom-wp_dashboard.php
@@ -22,16 +22,18 @@ remove_action( 'welcome_panel', 'wp_welcome_panel' );
 
 /*----------  Suppressions  ----------*/
 
-add_action( 'wp_dashboard_setup', function() {
+add_action( 'wp_dashboard_setup', function() use($pcSettings) {
 
     remove_meta_box('dashboard_right_now', 'dashboard', 'normal');   		// Right Now
-    remove_meta_box( 'dashboard_quick_press',   'dashboard', 'side' );      // Quick Press widget
-    //remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );    // Recent Drafts
-    remove_meta_box( 'dashboard_primary',       'dashboard', 'side' );      // WordPress.com Blog
-    remove_meta_box( 'dashboard_secondary',     'dashboard', 'side' );      // Other WordPress News
-    //remove_meta_box( 'dashboard_incoming_links','dashboard', 'normal' );  // Incoming Links
-    remove_meta_box( 'dashboard_plugins',       'dashboard', 'normal' );    // Plugins
-    remove_meta_box( 'dashboard_activity',   'dashboard', 'normal' );      	// activity
+    remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );        // Quick Press widget
+    remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side' );    // Recent Drafts
+    remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );            // WordPress.com Blog
+    remove_meta_box( 'dashboard_secondary', 'dashboard', 'side' );          // Other WordPress News
+    remove_meta_box( 'dashboard_incoming_links','dashboard', 'normal' );  // Incoming Links
+    remove_meta_box( 'dashboard_plugins', 'dashboard', 'normal' );          // Plugins
+    if ( !isset($pcSettings['comments-menu']) ) {
+        remove_meta_box( 'dashboard_activity', 'dashboard', 'normal' );      	// activity
+    }
 
 });
 
