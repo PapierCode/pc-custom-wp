d69fdbd3d1acbb0a24c283218843512fdaed5814
julien <julien@papier-code.fr>
2017-11-07 16:19:34 +0100

    V0.8.3 modification affichage image légendée (caption)

--- pc-custom-wp_medias.php
+++ pc-custom-wp_medias.php
@@ -9,11 +9,18 @@
 
 /*----------  Options par défaut  ----------*/
 
-add_action('after_setup_theme', 'custom_image_size');
+add_action( 'after_setup_theme', function() {
     
-    function custom_image_size() {
-    
-        // taille sélectionnée par défaut lors de l'ajout
-        update_option( 'image_default_size', 'large' );
+    // taille sélectionnée par défaut lors de l'ajout
+    update_option( 'image_default_size', 'large' );
 
-    }
+});
+
+
+/*----------  Caption  ----------*/
+
+add_filter( 'img_caption_shortcode', function( $empty, $attr, $content ) {
+
+    return '<figure class="wp-caption '.$attr['align'].'">'.$content.'<figcaption style="max-width:'.$attr['width'].'px">'.$attr['caption'].'</figcaption></figure>';
+
+}, 10, 3 );
