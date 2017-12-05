76572e2cee1ffa5c92b80e6c38682c30614dddcd
julien <julien@papier-code.fr>
2017-12-05 11:30:44 +0100

    V0.9.1 ajout exemple custom field pdf

--- class-examples/ex_add-metabox.php
+++ class-examples/ex_add-metabox.php
@@ -17,7 +17,7 @@ if ( class_exists('PC_Add_Metabox') ) {
 $tinymceDefault = get_option( 'pc-settings-option' );
 
 // contenu de la metabox
-$customMetaboxContent = array(	    
+$customMetaboxAContent = array(	    
     'desc'      => '<p>Un contenu HTML libre.</p>',
     'prefix'    => 'prefix',                                // obligatoire
     'fields'    => array(
@@ -83,7 +83,14 @@ $customMetaboxContent = array(
                 'Valeur B' => 'b',
                 'Valeur C' => 'c'
             )
-        ),
+        )
+    )
+);
+
+$customMetaboxBContent = array(     
+    'desc'      => '<p>Un contenu HTML libre.</p>',
+    'prefix'    => 'prefix',                                // obligatoire
+    'fields'    => array(
         array(
             'type'      => 'img',                           // obligatoire
             'id'        => 'img',                           // obligatoire
@@ -94,9 +101,18 @@ $customMetaboxContent = array(
             )
         ),
         array(
-            'type'      => 'gallery',                           // obligatoire
-            'id'        => 'gallery',                           // obligatoire
-            'label'     => 'Custom gallerie',                  // obligatoire
+            'type'      => 'pdf',                           // obligatoire
+            'id'        => 'pdf',                           // obligatoire
+            'label'     => 'Custom pdf',                    // obligatoire
+            'desc'      => 'Aide ou description du champ',
+            'options'   => array(                           // obligatoire
+                'btnremove' => true
+            )
+        ),
+        array(
+            'type'      => 'gallery',                       // obligatoire
+            'id'        => 'gallery',                       // obligatoire
+            'label'     => 'Custom gallerie',               // obligatoire
             'desc'      => 'Aide ou description du champ',
             'options'   => array(                           // obligatoire
                 'btnremove' => true
@@ -135,11 +151,18 @@ $customMetaboxContent = array(
 
 // cf. https://developer.wordpress.org/reference/functions/add_meta_box/
 
-$customMetabox = new PC_Add_Metabox(
+$customMetaboxA = new PC_Add_Metabox(
+    array( 'custompost' ), // obligatoire, posts concernés
+    'Custom Metabox A',      // obligatoire, titre
+    'custom-metabox-a',      // obligatoire, id
+    $customMetaboxAContent  // obligatoire, contenu
+);
+
+$customMetaboxB = new PC_Add_Metabox(
 	array( 'custompost' ), // obligatoire, posts concernés
-	'Custom Metabox',      // obligatoire, titre
-	'custom-metabox',      // obligatoire, id
-	$customMetaboxContent  // obligatoire, contenu
+	'Custom Metabox B',      // obligatoire, titre
+	'custom-metabox-b',      // obligatoire, id
+	$customMetaboxBContent  // obligatoire, contenu
 );
 
 
